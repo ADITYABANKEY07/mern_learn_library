e28149a91200681c1e18dfff31e48750a563d30e
bankeyaditya <bankeyaditya7@gmail.com>
2026-03-26 08:52:59 +0530

    adding logout feature and changing Navbar bg color

--- 17_task_management/client/src/components/Navbar.jsx
+++ 17_task_management/client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-around py-4 bg-blue-600 text-white">
+    <div className="flex items-center justify-around py-4 bg-gradient-to-r from-[#0897d4] via-[#090979] to-[#00d4ff] text-white">
     <h1 className="text-5xl font-bold" >Task Management</h1>
       {/* <div className="nav-link flex font-semibold gap-10 text-3xl">
         <Link to={"/home"}>Home</Link>
