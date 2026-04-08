15dc20e9ba492ed047d51f4ef073e20642b4d599
bankeyaditya <bankeyaditya7@gmail.com>
2026-04-08 23:56:29 +0530

    fix 404 middleware order

--- 17_task_management/server/app.js
+++ 17_task_management/server/app.js
@@ -5,19 +5,23 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const adminRoute = require("./route/adminRoute");
-const userRoute = require("./route/userRoute");
+const userRoute = require("./route/userRoute")
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5005;
 
-app.use(cors({
-  origin: 'https://task-manager-app-woad-ten.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "https://task-manager-app-woad-ten.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/admin", adminRoute)
-app.use("/user", userRoute)
+app.use("/admin", adminRoute);
+app.use("/user", userRoute);
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Db connected successfully!!!");
