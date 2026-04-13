4823e45f196ac6b9c1b961c31b534c88b9e9cff5
bankeyaditya <bankeyaditya7@gmail.com>
2026-04-13 22:34:29 +0530

    adding extra routes

--- 20_module_pkg/backend/controller/userController.js
+++ 20_module_pkg/backend/controller/userController.js
@@ -2,6 +2,9 @@ class userController{
   static get_session_info=async(req,res)=>{
     res.send("Welcome to My Session")
   }
+  static display_info=async(req,res)=>{
+    res.send("Welcome to My Display")
+  }
 }
 
 export default userController
--- 20_module_pkg/backend/route/userRoute.js
+++ 20_module_pkg/backend/route/userRoute.js
@@ -3,5 +3,6 @@ const route = express.Router()
 import userController from "../controller/userController.js"
 
 route.get("/getinfosession", userController.get_session_info)
+route.get("/displayinfo", userController.display_info)
 
 export default route
